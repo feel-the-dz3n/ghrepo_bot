54d40db05acbb25f41a6c22c9c448beec7982e5b
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-29 13:22:28 +0300

    Add project files.

---
+++ TelegramGitHubBot/Program.cs
@@ -0,0 +1,12 @@
+﻿``using System;
+
+namespace TelegramGitHubBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
